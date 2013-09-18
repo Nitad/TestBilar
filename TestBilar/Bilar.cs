0784e1412519a3e2dfd4c650a458104461742b31
Nitad <azash85@gmail.com>
2013-09-18 09:43:42 +0200

    Added class "Bilar" and some code in main.

---
+++ TestBilar/Bilar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Bilar
+{
+    public static int Wheels {get; set;}
+    public static int Horsepower { get; set; }
+
+    public Bilar(int wheels, int horsepower)
+    {
+        Wheels = wheels;
+        Horsepower = horsepower;
+    }
+    public int CountWheels()
+    {
+        return Wheels;
+    }
+    public int CountHorsepower()
+    {
+        return Horsepower;
+    }
+}
+
--- TestBilar/Program.cs
+++ TestBilar/Program.cs
@@ -10,6 +10,12 @@ namespace TestBilar
     {
         static void Main(string[] args)
         {
+            var saab = new Bilar(4, 90);
+            var BMW = new Bilar(4, 200);
+
+            Console.WriteLine(saab.CountWheels());
+            Console.WriteLine(BMW.CountHorsepower());
+
         }
     }
 }
